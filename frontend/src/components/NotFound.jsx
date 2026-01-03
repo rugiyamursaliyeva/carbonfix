85fd574d561c8eb269c23791b7579f220b99cda6
rugiyamursaliyeva <ruqiyemurseliyeva@gmail.com>
2026-01-03 22:14:46 +0400

    components and router add

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div style={{display:"flex", alignItems:"center", justifyContent:"center", padding:"20px"}}>
+      <img src={notfound} alt="" />
+    </div>
+  )
+}
+
+export default NotFound
