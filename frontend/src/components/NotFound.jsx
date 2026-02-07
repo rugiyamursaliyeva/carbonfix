5fded2e337255cee14073225dd66af1bcaa51e18
rugiyamursaliyeva <ruqiyemurseliyeva@gmail.com>
2026-02-07 16:17:21 +0400

    not found add

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div style={{display:"flex", alignItems:"center", justifyContent:"center", padding:"20px"}}>
-      <img src={notfound} alt="" />
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center", padding:"20px", width:"100%"}}>
+        <img src="https://static.vecteezy.com/system/resources/previews/008/255/803/non_2x/page-not-found-error-404-system-updates-uploading-computing-operation-installation-programs-system-maintenance-a-hand-drawn-layout-template-of-a-broken-robot-illustration-vector.jpg"
+        style={{width:"650px"}} alt="" />
     </div>
   )
 }
--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from '../components/Layout';
 import Home from '../pages/home/Home';
+import NotFound from '../components/NotFound';
 
 const Router = () => {
   return (
     <BrowserRouter>
     <Routes>
         <Route element={<Layout/>}>
-          <Route path='/' element={<Home/>}/>
-            
+          <Route path='/' element={<Home/>}/>       
         </Route>
+        <Route path='*' element={<NotFound/>}/>
     </Routes>
     </BrowserRouter>
   )
